6934489b56e712f843f233051c8e5ad7075c9a00
Yasutaka ATARASHI <yakex@cpan.org>
2013-04-20 16:45:12 +0900

    Move implementation to module, for testability (close gh-4).

--- lib/App/installdeps.pm
+++ lib/App/installdeps.pm
@@ -6,6 +6,51 @@ use warnings;
 # ABSTRACT: A tiny script to install dependent modules
 # VERSION
 
+use Getopt::Std;
+use Pod::Usage;
+
+use Module::ExtractUse;
+use File::Find;
+
+sub _process
+{
+	local (@ARGV) = @_;
+
+	my %opts;
+	getopts('hi:nx:', \%opts);
+	pod2usage(-verbosity => 2) if exists $opts{h};
+	pod2usage(-msg => 'At least one argument MUST be specified', -verbose => 0, -exitval => 1) if ! @ARGV;
+	$opts{i} ||= 'cpanm';
+
+	my $p = Module::ExtractUse->new;
+
+	while(my $arg = shift @ARGV) {
+		if(-f $arg) { $p->extract_use($arg); }
+		elsif(-d $arg) {
+			find({ no_chdir => 1, wanted => sub {
+				$p->extract_use($_) if -f $_;
+			}}, $arg);
+		} else {
+			warn "can't recognize argument: $arg";
+		}
+	}
+	my (@target) = grep { ! exists $opts{x} || $_ !~ /$opts{x}/ } grep { ! eval "use $_"; } keys %{$p->used};
+	return (\%opts, \@target);
+}
+
+sub run
+{
+	shift if @_ && eval { $_[0]->isa(__PACKAGE__) };
+	my ($opts, $target) = _process(@_);
+
+	if($opts->{n}) {
+		print join(' ', @$target), "\n";
+	} else {
+		print $opts->{i},' ',join(' ', @$target), "\n";
+#		system $opts->{i},@$target;
+	}
+}
+
 1;
 __END__
 
